185c5bfb8533fc788176308b1eb84565909ee96b
BugHunter-X <askzenai@gmail.com>
2024-12-19 19:46:07 +0100

    Add bugSolution.sol file

---
+++ bugSolution.sol
@@ -0,0 +1,37 @@
+function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
+
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction underflow");
+        uint256 c = a - b;
+        return c;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a == 0) {
+            return 0;
+        }
+        uint256 c = a * b;
+        require(c / a == b, "SafeMath: multiplication overflow");
+        return c;
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b > 0, "SafeMath: division by zero");
+        uint256 c = a / b;
+        return c;
+    }
+}
+
+contract MyContract {
+    using SafeMath for uint256;
+    mapping (address => uint256) balances;
+    function deposit(uint256 amount) public {
+        balances[msg.sender] = balances[msg.sender].add(amount);
+    }
+}
